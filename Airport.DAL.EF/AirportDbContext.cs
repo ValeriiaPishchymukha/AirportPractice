17f65688580bf0028f6eadd091d6197b54893f1b
ValeriiaPishchymukha <pishchymukha.valeriia@gmail.com>
2025-06-28 20:19:01 +0300

    Добавьте файлы проекта.

---
+++ Airport.DAL.EF/AirportDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Airport.DAL.EF.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport.DAL.EF
+{
+    public class AirportDbContext : DbContext
+    {
+        public AirportDbContext(DbContextOptions<AirportDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Passenger> Passengers { get; set; }
+
+    }
+}
---
+++ WebApplicationAirport/Program.cs
@@ -0,0 +1,41 @@
+using Airport.DAL.EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationAirport
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AirportDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
